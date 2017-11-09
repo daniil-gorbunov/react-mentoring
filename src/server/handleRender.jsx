645e3b38848291c5c904b52c3568bddd1399f280
Daniil Gorbunov <daniil_gorbunov@epam.com>
2017-11-09 19:36:05 +0300

    base ssr

--- src/server/handleRender.jsx
+++ src/server/handleRender.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { StaticRouter as Router } from 'react-router-dom';
 import App from '../client/components/App/index';
 
 function renderFullPage(html) {
@@ -7,7 +8,7 @@ function renderFullPage(html) {
     <!doctype html>
     <html lang="en">
       <head>
-        <meta>(charset="UTF-8")</head>
+        <meta charset="UTF-8"></head>
         <title>Netflix Roulette</title>
       </head>
       <body>
@@ -18,8 +19,17 @@ function renderFullPage(html) {
 }
 
 function handleRender(req, res) {
-  const html = renderToString(<App />);
-  res.send(renderFullPage(html));
+  const context = {};
+  const app = (
+    <Router location={req.url} context={context} >
+      <App />
+    </Router>
+  );
+  const html = renderToString(app);
+  if (context.url) {
+    return res.redirect(context.url);
+  }
+  return res.send(renderFullPage(html));
 }
 
 export default handleRender;
